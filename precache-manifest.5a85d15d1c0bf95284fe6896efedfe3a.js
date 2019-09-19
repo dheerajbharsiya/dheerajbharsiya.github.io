ad09c8c3a6487934688bcc3c27b82a8d72eb3c54
dheeraj <dheeraj.bharsiya@broadcom.com>
2019-09-19 17:03:39 +0530

    git addding new react

---
+++ precache-manifest.5a85d15d1c0bf95284fe6896efedfe3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52a00989da02bc00a6dc",
+    "url": "/static/css/main.443fe0e0.chunk.css"
+  },
+  {
+    "revision": "52a00989da02bc00a6dc",
+    "url": "/static/js/main.52a00989.chunk.js"
+  },
+  {
+    "revision": "cf99dc7e1619ffa50f53",
+    "url": "/static/css/1.60bcad7b.chunk.css"
+  },
+  {
+    "revision": "cf99dc7e1619ffa50f53",
+    "url": "/static/js/1.cf99dc7e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b9deb6e543330da0c29d4706c011995",
+    "url": "/index.html"
+  }
+];
